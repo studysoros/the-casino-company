2c016e540d50d84bc0f2cade72007a69d797e81f
fatsummercat <homelnw77@gmail.com>
2025-12-02 23:40:28 +0700

    feat: frontend API structure

---
+++ shared/contracts/http.go
@@ -0,0 +1,11 @@
+package contracts
+
+type APIResponse struct {
+	Data  any       `json:"data,omitempty"`
+	Error *APIError `json:"error,omitempty"`
+}
+
+type APIError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
